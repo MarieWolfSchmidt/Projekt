3b3f33b46607016b9011771b5d615184aad220d8
ulfur88 <ulfur88@gmail.com>
2017-01-10 10:46:55 +0100

    Add files via upload

---
+++ Mads' Folder/src/Model/CheckersModel.java
@@ -0,0 +1,67 @@
+package Model;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import View.CheckersView;
+
+public class CheckersModel {
+
+	private CheckersView view;
+	private List<Pieces> pieceList;
+	private MovePieces move;
+	
+	private int boardSize = 800;
+	private int squareDim;
+	protected int finalBoardSize; // If the supplied commandLineArgument is an odd number then we change the boardSize so it matches.
+	private int pieceSize;
+	
+	
+	public CheckersModel(CheckersView view) {
+		this.view = view;
+    	this.squareDim = boardSize/view.cmdLineArgument; 
+    	this.finalBoardSize = squareDim*view.cmdLineArgument;
+    	this.pieceSize = (squareDim/3)*2;
+    	this.pieceList = new ArrayList<>();
+    	this.move = new MovePieces(pieceList, squareDim, pieceSize, finalBoardSize);
+	}
+	
+	public void game() {
+    	add(1, 1, 1, squareDim, pieceList);
+    	add(2, view.cmdLineArgument, view.cmdLineArgument, squareDim, pieceList);
+		this.view.menu.game(view.cmdLineArgument, pieceList, boardSize, squareDim, finalBoardSize, pieceSize);
+	}
+	
+	public void menu() {
+		this.view.menu.menu();
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		move.pressed(e);
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		move.mouseDragged(e, view.menu.draw);
+	}
+	
+	public void mouseReleased(MouseEvent e) {
+		move.released(e, view.menu.draw);
+	}
+	
+	public void add(int i, int row, int col, int squareDim, List<Pieces> pieceList) {
+		
+		// Instantiates a piece and adds it to the pieceList. 
+		
+		Point p = new Point((col-1)*squareDim+squareDim/2,(row-1)*squareDim+squareDim/2); 
+		Pieces piece = new Pieces(i,p);
+	    pieceList.add(piece);     
+	}
+	
+	public void removePieces() {
+		
+		// Removes all pieces from the pieceList when the user presses the back to menu button in the game() menu.
+			this.pieceList.clear();
+	}
+	
+}
---
+++ Mads' Folder/src/View/Draw.java
@@ -0,0 +1,73 @@
+package View;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.List;
+
+import javax.swing.JComponent;
+
+import Model.Pieces;
+
+public class Draw extends JComponent {
+	
+	private int boardSize;
+	private int squareDim;
+	protected int finalBoardSize; // If the supplied commandLineArgument is an odd number then we change the boardSize so it matches.
+	private int pieceSize;
+	private List<Pieces> pieceList; 
+	
+	public Draw(List<Pieces> pieceList, int boardSize, int squareDim, int finalBoardSize, int pieceSize){
+    	this.pieceList = pieceList; 
+    	this.boardSize = boardSize;
+    	this.squareDim = squareDim;
+    	this.finalBoardSize = finalBoardSize;
+    	this.pieceSize = pieceSize; 
+	}
+	
+	
+	 protected void paintComponent(Graphics g) {
+		 // Override jComponent's function paintComponent to draw the board and the pieces.
+	     // The method draws the board on a JPanel utilizing the structure of a coordinate system.
+	     // It begins in (0,0) and draws two rows at a time to easier manipulate the colors of the board.
+	     // The size of the squares are decided using the size of the board divided by the cmdlineargument. 
+	        
+	     for(int i = 0; i <= boardSize - squareDim; i += 2*squareDim) { 		// 2*squareDim is added to i to fill in the right color.
+	        for(int j = 0; j <= boardSize - squareDim; j += 2*squareDim) {
+	        	g.setColor(Color.GRAY);
+	        	g.fillRect(j, i, squareDim, squareDim);						// Gray square - first row.
+	        	g.setColor(Color.WHITE);
+	        	g.fillRect(j+squareDim, i, squareDim, squareDim); 			// White square - first row.
+	        	g.setColor(Color.GRAY);
+	        	g.fillRect(j+squareDim, i+squareDim, squareDim, squareDim);	// Gray square - second row.
+	        	g.setColor(Color.WHITE);
+	        	g.fillRect(j, i+squareDim, squareDim, squareDim);			// White square - second row.
+	        }
+	     }
+	            
+	     // Draws a black rectangle to get a black square around the board. 
+	     g.setColor(Color.BLACK);
+	     // If the supplied cmdLineArgument is odd one will have to calculate the finalBoardSize, because of ints 
+	     // way of rounding down. Otherwise one would end up with a little bit of extra board.
+	     g.drawRect(0, 0, finalBoardSize, finalBoardSize); 
+	            
+	     // Draws a line around all the squares.
+	     for (int i = 0; i < finalBoardSize; i += squareDim) {
+	    	 g.drawLine(i, 0, i, finalBoardSize);
+	     }
+	     for (int i = 0; i < finalBoardSize; i += squareDim) {
+	         g.drawLine(0, i, finalBoardSize, i);
+	     }	
+	            
+	     // Adds pieces.
+	     for(Pieces piece: pieceList) {
+	    	 piece.drawPieces(g, piece.point, pieceSize);
+	     }
+	           	
+	 }
+	 
+	 public void repaintBoardAndPieces() {
+		 // Calls paintComponent()
+		 repaint();   
+	 }
+	 
+}
